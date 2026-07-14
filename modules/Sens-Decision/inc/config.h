e86ae56ed3b3a02bbc830110384af9d8602d675f
Joelin-CN <linlinepm@gmail.com>
2026-07-14 20:20:00 +0800

    0714 1th update

---
+++ modules/Sens-Decision/inc/config.h
@@ -0,0 +1,10 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+
+
+
+
+
+
+#endif // CONFIG_H
---
+++ modules/Sens-Decision/inc/interface.h
@@ -0,0 +1,24 @@
+/**
+ * @file      interface.h
+ * @brief     Sens-Decision所有的硬件接口的实现
+ * @author    joelin-CN
+ * @version   1.0.0
+ * @date      2026-07-14
+ * @note      Sens-Decision所有的硬件接口
+ */
+#ifndef INTERFACE_H
+#define INTERFACE_H
+
+
+struct interface
+{
+    /* data */
+};
+
+
+
+
+
+
+
+#endif
